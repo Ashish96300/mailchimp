a1ffecb55827136fb3716c0416d5412a4eefd8b4
Ashish Sen <ashishsen2609@gmail.com>
2026-01-06 12:16:30 +0530

    created all tables

---
+++ migrations/001_init.sql
@@ -0,0 +1,68 @@
+CREATE TABLE users(
+    id BIGSERIAL PRIMARY KEY,
+
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+
+);
+
+CREATE TABLE audiences(
+    id BIGSERIAL PRIMARY KEY,
+
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    name TEXT NOT NULL,
+    description TEXT,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+
+);
+
+CREATE TABLE contacts(
+     id BIGSERIAL PRIMARY KEY,
+
+     audience_id BIGINT NOT NULL REFERENCES audiences(id) ON DELETE CASCADE,
+     name TEXT,
+     email TEXT NOT NULL,
+     status TEXT NOT NULL DEFAULT 'subscribed',
+     
+     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+     UNIQUE (audience_id, email) /*This prevents duplicate emails in the same audience, but allows the same email in different audiences.*/
+);
+
+CREATE TABLE campaigns (
+    id BIGSERIAL PRIMARY KEY,
+
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    audience_id BIGINT NOT NULL REFERENCES audiences(id) ON DELETE CASCADE,
+
+    subject TEXT NOT NULL,
+    body TEXT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'draft',
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    sent_at TIMESTAMPTZ
+);
+
+CREATE TABLE email_jobs (
+    id BIGSERIAL PRIMARY KEY,
+
+    campaign_id BIGINT NOT NULL REFERENCES campaigns(id) ON DELETE CASCADE,
+    contact_id BIGINT NOT NULL REFERENCES contacts(id) ON DELETE CASCADE,
+
+    to_email TEXT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'pending',
+    retry_count INT NOT NULL DEFAULT 0,
+    last_error TEXT,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    sent_at TIMESTAMPTZ
+);
