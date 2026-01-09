b673ecad1c8e3803decea42f1d7b54ff67892c51
Ashish Sen <ashishsen2609@gmail.com>
2026-01-09 12:58:00 +0530

    repo.go for audience

---
+++ internals/modules/audience/repository.go
@@ -0,0 +1,13 @@
+package audience
+
+import(
+	"context"
+)
+
+type Repository interface{
+	Create(ctx context.Context ,audience *Audience) error
+	GetById(ctx context.Context ,id int64)(*Audience ,error)
+	ListByUser(ctx context.Context ,userID int64)([]Audience ,error)
+	Update(ctx context.Context ,audience *Audience) error
+	Delete(ctx context.Context ,id int64)error
+}
