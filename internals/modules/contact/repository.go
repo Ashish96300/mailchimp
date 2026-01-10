f3e4ac440ae2b35ba1eabbab3754c1689edd47b5
Ashish Sen <ashishsen2609@gmail.com>
2026-01-10 22:32:45 +0530

    repo.go for contacts

---
+++ internals/modules/contact/repository.go
@@ -0,0 +1,28 @@
+package contact
+
+import "context"
+
+type Repository interface {
+	Create(ctx context.Context, contact *Contact) error
+
+	GetById(ctx context.Context, id int64) (*Contact, error)
+
+	ListByAudience(
+		ctx context.Context,
+		audienceID int64,
+	) ([]Contact, error)
+
+	GetByEmail(
+		ctx context.Context,
+		audienceID int64,
+		email string,
+	) (*Contact, error)
+
+	UpdateStatus(
+		ctx context.Context,
+		id int64,
+		status ContactStatus,
+	) error
+
+	Delete(ctx context.Context, id int64) error
+}
