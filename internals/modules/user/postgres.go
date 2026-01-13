fc4618f36a297c0192d3a089a383f07b26b4a924
Ashish Sen <ashishsen2609@gmail.com>
2026-01-13 22:13:02 +0530

    remaining func of user postgres.go

--- internals/modules/user/postgres.go
+++ internals/modules/user/postgres.go
@@ -65,3 +65,67 @@ func (r *postgresRepository) GetById(
 
 	return &user, nil
 }
+
+func (r *postgresRepository) GetByEmail(
+	ctx context.Context,
+	email string,
+) (*User, error) {
+
+	query := `
+		SELECT id, name, email, password_hash, created_at, updated_at
+		FROM users
+		WHERE email = $1;
+	`
+
+	var user User
+
+	err := r.db.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.PasswordHash,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *postgresRepository) UpdatePassword(
+	ctx context.Context,
+	id int64,
+	passwordHash string,
+) error {
+
+	query := `
+		UPDATE users
+		SET password_hash = $2, updated_at = NOW()
+		WHERE id = $1;
+	`
+
+	_, err := r.db.ExecContext(
+		ctx,
+		query,
+		id,
+		passwordHash,
+	)
+
+	return err
+}
+func (r *postgresRepository) Delete(
+	ctx context.Context,
+	id int64,
+) error {
+
+	query := `
+		DELETE FROM users
+		WHERE id = $1;
+	`
+
+	_, err := r.db.ExecContext(ctx, query, id)
+	return err
+}
+
